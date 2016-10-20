1ae675b62332267474861da3a01498d260ed2e28
Austin Blain and Eric Raetz <austin@>
2016-10-20 12:00:09 -0700

    add functioning nonbootstrap alert

--- js/script.js
+++ js/script.js
@@ -5,19 +5,27 @@ $(document).ready(function() {
 
   var social = $("#social").val();
   var economic = $("#economic").val();
+  var age = parseInt($("input#age").val());
 
-   if (social === "My way or death" && economic === "All mine") {
+  // if (age =< 15) {
+  //   $("#age").addClass(".has-error")
+  //   alert("too young");
+  // }
+
+   if (social === "My way or death" && economic === "All mine" && (100 >= age, age >= 15)) {
      $(".solution").text("Flaming Anarchist")
      $('#results').show();
-   } else if (social === "My way or death" && economic === "All yours") {
+   } else if (social === "My way or death" && economic === "All yours"  && (100 >= age, age >= 15)) {
      $(".solution").text("Flaming communist")
      $('#results').show();
-   } else if (social === "Your way or death" && economic === "All mine") {
+   } else if (social === "Your way or death" && economic === "All mine" && (100 >= age, age >= 15)) {
      $(".solution").text("Very confused")
      $('#results').show();
-   } else {
+   } else if (social === "Your way or death" && economic === "All yours" && (100 >= age, age >= 15)) {
      $(".solution").text("Suicidal")
      $('#results').show();
+   } else {
+     alert("Enter age (must be 16 or older)!");
    }
    event.preventDefault();
    });
